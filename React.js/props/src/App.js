2e1bea14c37f7a6aaca7a24a0def0c27870d6ed6
vijay solanki <vijaysolanki1481@gmail.com>
2024-07-05 12:04:18 +0530

    5-jul

--- React.js/props/src/App.js
+++ React.js/props/src/App.js
@@ -12,9 +12,9 @@ function App() {
     <div className="App">
       <div className="text-center p-10">
         <h1 className="font-bold text-4xl mb-4">
-          Responsive Product card grid
+          My Phone Shop
         </h1>
-        <h1 className="text-3xl">Tailwind CSS</h1>
+        <h1 className="text-3xl">Victor Dizhard</h1>
       </div>
 
       <div className="w-fit mx-auto grid grid-cols-1 lg:grid-cols-3 md:grid-cols-2 justify-items-center gap-y-20 gap-x-14 mt-10 mb-5 justify-center">
