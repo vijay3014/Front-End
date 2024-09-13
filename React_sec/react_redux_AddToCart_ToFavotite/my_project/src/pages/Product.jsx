e445472043133463693d6bfee93dde6cc33cd6eb
vijay <vijaysolanki1481@gmail.com>
2024-09-13 11:17:27 +0530

    13-sep

---
+++ React_sec/react_redux_AddToCart_ToFavotite/my_project/src/pages/Product.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Product = () => {
+  return (
+    <div className='h-screen bg-gray-800'>
+        <h1 className='heading'>This is Product Page</h1>
+    </div>  
+  )
+}
+
+export default Product
